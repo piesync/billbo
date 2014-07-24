e945c851c8e451774e50513fbef8e7858859bc5c
Mattias Putman <mattias.putman@gmail.com>
2014-07-24 20:48:33 +0200

    configure vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
 require 'minitest/spec'
 require 'minitest/autorun'
 
+$environment = :test
 require './environment'
+
+# Configure VCR.
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = {
+    record: :once,
+    allow_unused_http_interactions: false
+  }
+end
