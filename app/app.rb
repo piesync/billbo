00e34022d945c7dc978da7d71248e663130efddc
Mattias Putman <mattias.putman@gmail.com>
2014-07-28 16:12:29 +0200

    endpoint to get price preview

--- app/app.rb
+++ app/app.rb
@@ -17,6 +17,35 @@ class App < Base
     json(subscription)
   end
 
+  # Fetches a preview breakdown of the costs of a subscription.
+  #
+  # plan         - Stripe plan ID.
+  # country_code - Country code of the customer (ISO 3166-1 alpha-2 standard)
+  # is_company   - Whether the customer is a company (default: false).
+  #
+  # Returns 200 and
+  # {
+  #    subtotal: 10,
+  #    currency: 'eur',
+  #    vat: 2.1,
+  #    vat_rate: '21'
+  # }
+  get '/preview/:plan' do
+    plan = Stripe::Plan.retrieve(params[:plan])
+
+    vat = vat_service.calculate \
+      amount: plan.amount,
+      country_code: params[:country_code],
+      is_company: (params[:is_company] == 'true')
+
+    json({
+      subtotal: plan.amount,
+      currency: plan.currency,
+      vat: vat.amount,
+      vat_rate: vat.rate
+    })
+  end
+
   get '/vat/:number' do
     if vat_service.valid?(vat_number: params[:number])
       status 200
