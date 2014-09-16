7ed26464a743839e4206c035137e5d6ab870c4b6
Mattias Putman <mattias.putman@gmail.com>
2014-09-16 23:57:49 +0200

    PdfService#generate_missing

--- lib/pdf_service.rb
+++ lib/pdf_service.rb
@@ -9,7 +9,13 @@ class PdfService
       "http://X:#{Configuration.api_token}@#{Configuration.host}/invoices/#{invoice.id}")
     phantom.to_pdf("#{invoice.id}.pdf")
 
-    uploader.store!(File.open("#{invoice.id}.pdf"))
+    Invoice.pdf_generated! if uploader.store!(File.open("#{invoice.id}.pdf"))
+  end
+
+  def generate_missing
+    Invoice.where(pdf_generated_at: nil).each do |invoice|
+      generate_pdf(invoice)
+    end
   end
 
   private
